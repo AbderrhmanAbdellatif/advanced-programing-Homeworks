1e62e7f52ba2dc895a495c5e748a621b7a46fde1
Abderhman Abdelatif <taha196tr@gmail.com>
2018-10-24 15:12:40 +0300

    LAB5

---
+++ Lab/work/Database.js
@@ -0,0 +1,19 @@
+class course{
+	constructor(name ,time ,data,rooms){
+		this.name=name
+		this.time=time
+		this.data=data
+		this.rooms=room
+	}
+	
+	
+}
+class Student{
+	constructor(id,name ,gpa,course){
+		this.id=id
+		this.name=name
+		this.gpa=gpa
+		this.course=course
+	}	
+}
+
