75d9623f87c61db18364f950373b07ed007055aa
Abderhman Abdelatif <taha196tr@gmail.com>
2018-10-28 14:40:06 +0300

    Hw

--- Lab/HomeWorks/Database.js
+++ Lab/HomeWorks/Database.js
@@ -21,40 +21,49 @@ class Student {
 
 }
 class Database {
-
-    constructor() {
-
-    }
+    constructor() {}
     addstudent(s = []) {
         var studentMap = new Map();
-
         for (let index = 0; index < s.length; index++) {
             this.studensset = new Student();
             for (let i = 0; i < s[index].length; i++) {
-
-
                 if (i == 0) {
                     this.studensset.id = parseInt(s[index][i]);
-
                 }
                 if (i == 1) {
                     this.studensset.name = (s[index][i]);
-
                 }
                 if (i == 2) {
                     this.studensset.gpa = s[index][i];
-
                 }
                 if (i > 2) {
                     this.studensset.courses.push(s[index][i]);
-
                 }
-                studentMap.set(this.studensset.id, this.studensset.name + " , " + this.studensset.gpa + " , " + this.studensset.courses);
+                studentMap.set(this.studensset.id, this.studensset.name + " | " + this.studensset.gpa + " | " + this.studensset.courses);
             }
-
         }
-        for (var [key, value] of studentMap) {
-            console.log(key + ' => ' + value);
+        return studentMap;
+    }
+    addcourses(c = []) {
+        var coursesMap = new Map();
+        for (let index = 0; index < c.length; index++) {
+            this.coursesset = new Course();
+            for (let i = 0; i < c[index].length; i++) {
+                if (i == 0) {
+                    this.coursesset.name = c[index][i];
+                }
+                if (i == 1) {
+                    this.coursesset.time = c[index][i];
+                }
+                if (i == 2) {
+                    this.coursesset.date = c[index][i];
+                }
+                if (i > 2) {
+                    this.coursesset.rooms.push(c[index][i]);
+                }
+                coursesMap.set(this.coursesset.name, this.coursesset.time + " | " + this.coursesset.date + " | " + this.coursesset.rooms);
+            }
         }
+        return coursesMap;
     }
 }
