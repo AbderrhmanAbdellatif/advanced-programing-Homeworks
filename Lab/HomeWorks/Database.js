eda7a3839e8d6dbd713ee656eb0d0ede5cf6e50a
Abderhman Abdelatif <taha196tr@gmail.com>
2018-10-28 22:27:29 +0300

    HW

--- Lab/HomeWorks/Database.js
+++ Lab/HomeWorks/Database.js
@@ -1,6 +1,6 @@
 class Course {
 
-    constructor(name = "", time = "", date = "", rooms = []) {
+    constructor(name="", time="", date="", rooms=[]) {
         this.name = name;
         this.time = time;
         this.date = date;
@@ -8,16 +8,14 @@ class Course {
 
     }
 
-
 }
 class Student {
-    constructor(id = 0, name = "", gpa = 0, courses = []) {
+    constructor(id=0, name="", gpa=0, courses=[]) {
         this.id = id
         this.name = name
         this.gpa = gpa
         this.courses = courses
     }
-
 
 }
 class Database {
@@ -26,14 +24,14 @@ class Database {
         this.arrycor = [];
 
     }
-    addstudent(s = []) {
+    addstudent(s=[]) {
         var studentMap = new Map();
         for (let index = 0; index < s.length; index++) {
-        
+
             this.studensset = new Student();
-            
-             this.arrystu.push(this.studensset);
-        
+
+            this.arrystu.push(this.studensset);
+
             for (let i = 0; i < s[index].length; i++) {
                 if (i == 0) {
                     this.studensset.id = parseInt(s[index][i]);
@@ -41,11 +39,11 @@ class Database {
                 }
                 if (i == 1) {
                     this.studensset.name = (s[index][i]);
-                    this.arrystu[index].name=(s[index][i]);
+                    this.arrystu[index].name = (s[index][i]);
                 }
                 if (i == 2) {
                     this.studensset.gpa = s[index][i];
-                    this.arrystu[index].gpa=s[index][i];
+                    this.arrystu[index].gpa = s[index][i];
                 }
                 if (i > 2) {
                     this.studensset.courses.push(s[index][i] + "\t");
@@ -56,23 +54,23 @@ class Database {
         }
         return studentMap;
     }
-    addcourses(c = []) {
+    addcourses(c=[]) {
         var coursesMap = new Map();
         for (let index = 0; index < c.length; index++) {
             this.coursesset = new Course();
-                   this.arrycor.push(this.coursesset);
+            this.arrycor.push(this.coursesset);
             for (let i = 0; i < c[index].length; i++) {
                 if (i == 0) {
                     this.coursesset.name = c[index][i];
-                    this.arrycor[index].name= c[index][i];
+                    this.arrycor[index].name = c[index][i];
                 }
                 if (i == 1) {
                     this.coursesset.time = c[index][i];
-                    this.arrycor[index].time=c[index][i];
+                    this.arrycor[index].time = c[index][i];
                 }
                 if (i == 2) {
                     this.coursesset.date = c[index][i];
-                    this.arrycor[index].date=c[index][i];
+                    this.arrycor[index].date = c[index][i];
                 }
                 if (i > 2) {
                     this.coursesset.rooms.push(c[index][i]);
@@ -84,21 +82,19 @@ class Database {
         return coursesMap;
     }
 
-   bubbleSort(a) {
-     var swapped;
-     do {
-        swapped = false;
-        for (var i=0; i < a.length-1; i++) {
-            if (a[i].gpa > a[i+1].gpa) {
-                var temp = a[i];
-                a[i] = a[i+1];
-                a[i+1] = temp;
-                swapped = true;
+    bubbleSort(a) {
+        var swapped;
+        do {
+            swapped = false;
+            for (var i = 0; i < a.length - 1; i++) {
+                if (a[i].id > a[i + 1].id) {
+                    var temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                    swapped = true;
+                }
             }
-        }
-    } while (swapped);
-}
-
-
+        } while (swapped);
+    }
 
 }
