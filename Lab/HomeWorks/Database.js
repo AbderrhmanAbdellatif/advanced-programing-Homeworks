919a12307d8e083fdbe615c755eded7743ae24c5
Abderhman Abdelatif <taha196tr@gmail.com>
2018-10-28 13:50:33 +0300

    hw

--- Lab/HomeWorks/Database.js
+++ Lab/HomeWorks/Database.js
@@ -23,10 +23,38 @@ class Student {
 class Database {
 
     constructor() {
-        this.Filedata = "";
+
     }
+    addstudent(s = []) {
+        var studentMap = new Map();
+
+        for (let index = 0; index < s.length; index++) {
+            this.studensset = new Student();
+            for (let i = 0; i < s[index].length; i++) {
 
 
+                if (i == 0) {
+                    this.studensset.id = parseInt(s[index][i]);
 
+                }
+                if (i == 1) {
+                    this.studensset.name = (s[index][i]);
 
+                }
+                if (i == 2) {
+                    this.studensset.gpa = s[index][i];
+
+                }
+                if (i > 2) {
+                    this.studensset.courses.push(s[index][i]);
+
+                }
+                studentMap.set(this.studensset.id, this.studensset.name + " , " + this.studensset.gpa + " , " + this.studensset.courses);
+            }
+
+        }
+        for (var [key, value] of studentMap) {
+            console.log(key + ' => ' + value);
+        }
+    }
 }
