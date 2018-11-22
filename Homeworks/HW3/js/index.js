dda5cc3e644ae6e0b426687e69fbfe21068ea323
Suleiman Taha <dexter196tr@gmail.com>
2018-11-22 15:29:20 +0300

    HW3

--- Homeworks/HW3/js/index.js
+++ Homeworks/HW3/js/index.js
@@ -1,14 +1,3 @@
-var tl = new TimelineMax();
-$('.play').click(function() {
-  var tl = new TimelineMax();
-
-tl.from('#stand', 0.5, {scaleY: 0, transformOrigin: "bottom"})
-  .from('#standBack', 0.5, {scaleY: 0, transformOrigin: "bottom"})
-  .from('#monitorBottom', 0.7, {scaleX: 0, transformOrigin: "center"})
-  .from('#screen', 0.6, {scaleY: 0, transformOrigin: "bottom", ease: Circ.easeOut, delay: 0.4})
-  .from('#yellowBox', 0.5, {scale: 0})
-  .staggerFrom('#Layer_1 > g:nth-child(1) > g path', 0.2, {scaleX: 0}, -0.1)
-})
 
 document.getElementById("tunnel").animate([
 { transform: 'translate3D(0, 0, 0)' },
