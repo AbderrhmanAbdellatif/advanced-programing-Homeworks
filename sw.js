93abc59877ffa96f4826a0836ef317ea15ee3180
Suleiman Taha <dexter196tr@gmail.com>
2018-11-22 15:41:46 +0300

    update

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE ='JS'
+
+function save(req, resp) {
+  return caches.open(CACHE)
+  .then(cache => {
+    cache.put(req, resp.clone());
+    return resp;
+  })
+  .catch(console.log)
+}
+function fetchCB(e) { //fetch first
+  let req = e.request
+  console.log('BLM', req.url);
+  e.respondWith(
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => { return caches.match(req).then(r1 => r1) })
+  )
+}
+self.addEventListener('fetch', fetchCB)
