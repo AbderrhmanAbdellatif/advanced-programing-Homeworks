489afb00cb61c851ebe197de60eb0680aa3b051c
Suleiman Taha <dexter196tr@gmail.com>
2018-11-27 15:21:17 +0300

    HomeWork

--- sw.js
+++ sw.js
@@ -1,19 +1,16 @@
 const CACHE ='14MST'
-function installCB(e) {
-  console.log('install', e.request);
-}
-self.addEventListener('install', installCB)
+
 function save(req, resp) {
   return caches.open(CACHE)
   .then(cache => {
     cache.put(req, resp.clone());
     return resp;
-  })
+  }) 
   .catch(console.log)
 }
 function fetchCB(e) { //fetch first
   let req = e.request
-  console.log(CACHE, req.url);
+  console.log('A0M0RAJAB', req.url);
   e.respondWith(
     fetch(req).then(r2 => save(req, r2))
     .catch(() => { return caches.match(req).then(r1 => r1) })
