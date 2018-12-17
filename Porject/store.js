c9761aa266e529117526b9bd964c102baa7780e7
Suleiman Taha <dexter196tr@gmail.com>
2018-12-17 19:04:30 +0300

    update

--- Porject/store.js
+++ Porject/store.js
@@ -3,7 +3,7 @@ if (document.readyState == 'loading') {
 } else {
     ready()
 }
-	var arrayofAlbum=["Album 1","Album 2","Album 3","Album 4"];
+var arrayofAlbum = ["Album 1", "Album 2", "Album 3", "Album 4"];
 
 function ready() {
     var removeCartItemButtons = document.getElementsByClassName('btn-danger')
@@ -23,43 +23,44 @@ function ready() {
         var button = addToCartButtons[i]
         button.addEventListener('click', addToCartClicked)
     }
-		document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
-	
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+
 }
 
 function purchaseClicked() {
-	var size = document.getElementsByClassName("cart-item cart-column").length
-    if((size-1)==0){
-		    alert(' the shopping cart is empty :(')
-    }else{
-    alert('your purchase is successful \n thanks for your time (:')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    while (cartItems.hasChildNodes()) {
-        cartItems.removeChild(cartItems.firstChild)
+    var size = document.getElementsByClassName("cart-item cart-column").length
+    if ((size - 1) == 0) {
+        alert(' the shopping cart is empty :(')
+    } else {
+        alert('your purchase is successful \n thanks for your time (:')
+        var cartItems = document.getElementsByClassName('cart-items')[0]
+        while (cartItems.hasChildNodes()) {
+            cartItems.removeChild(cartItems.firstChild)
+        }
+        updateCartTotal()
     }
-    updateCartTotal()
-	}
 }
 
 function removeCartItem(event) {
     var buttonClicked = event.target
-    if(arrayofAlbum.includes(buttonClicked.parentElement.innerText.split("\n")[0])){
-		buttonClicked.parentElement.remove();
-	}else{
-		  buttonClicked.parentElement.parentElement.remove();
-	}
-	var arrayofitem=[];
+    if (arrayofAlbum.includes(buttonClicked.parentElement.innerText.split("\n")[0])) {
+        buttonClicked.parentElement.remove();
+    } else {
+        buttonClicked.parentElement.parentElement.remove();
+    }
+    var arrayofitem = [];
     var size = document.getElementsByClassName("cart-item cart-column").length
-    for(let i=0;i<size ;i++){
-		arrayofitem.push(document.getElementsByClassName("cart-item cart-column")[i].innerText);
-	}
-	for(let i=0;i<size ;i++){
-    if(!arrayofAlbum.includes(arrayofitem[i])){
-          updateCartTotal()
-	}if(!arrayofAlbum.includes(arrayofitem[i])){
-		console.log("Album ");
-	}
-	}
+    for (let i = 0; i < size; i++) {
+        arrayofitem.push(document.getElementsByClassName("cart-item cart-column")[i].innerText);
+    }
+    for (let i = 0; i < size; i++) {
+        if (!arrayofAlbum.includes(arrayofitem[i])) {
+            updateCartTotal()
+        }
+        if (!arrayofAlbum.includes(arrayofitem[i])) {
+            console.log("Album ");
+        }
+    }
 }
 
 function quantityChanged(event) {
@@ -67,20 +68,21 @@ function quantityChanged(event) {
     if (isNaN(input.value) || input.value <= 0) {
         input.value = 1
     }
-	var arrayofitem=[];
+    var arrayofitem = [];
     var size = document.getElementsByClassName("cart-item cart-column").length
-    for(let i=0;i<size ;i++){
-		arrayofitem.push(document.getElementsByClassName("cart-item cart-column")[i].innerText);
-	}
-	for(let i=0;i<size ;i++){
-    if(!arrayofAlbum.includes(arrayofitem[i])){
-          updateCartTotal()
-	}if(!arrayofAlbum.includes(arrayofitem[i])){
-		console.log("Album ");
-	}
-	}
+    for (let i = 0; i < size; i++) {
+        arrayofitem.push(document.getElementsByClassName("cart-item cart-column")[i].innerText);
+    }
+    for (let i = 0; i < size; i++) {
+        if (!arrayofAlbum.includes(arrayofitem[i])) {
+            updateCartTotal()
+        }
+        if (!arrayofAlbum.includes(arrayofitem[i])) {
+            console.log("Album ");
+        }
+    }
 
- }
+}
 function addToCartClicked(event) {
     var button = event.target
     var shopItem = button.parentElement.parentElement
@@ -88,26 +90,26 @@ function addToCartClicked(event) {
     var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
     var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
     addItemToCart(title, price, imageSrc)
-	//if(!arrayofAlbum.includes(title)){
+    //if(!arrayofAlbum.includes(title)){
     updateCartTotal()
-	//}else{
-//		console.log("Album ");
-	//}
+    //}else{
+    //		console.log("Album ");
+    //}
 }
 
 function addItemToCart(title, price, imageSrc) {
-	if(!arrayofAlbum.includes(title)){
-     var cartRow = document.createElement('div')
-     cartRow.classList.add('cart-row')
-     var cartItems = document.getElementsByClassName('cart-items')[0]
-     var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
-     for (var i = 0; i < cartItemNames.length; i++) {
-        if (cartItemNames[i].innerText == title) {
-            alert('This item is already added to the cart')
-            return
+    if (!arrayofAlbum.includes(title)) {
+        var cartRow = document.createElement('div')
+        cartRow.classList.add('cart-row')
+        var cartItems = document.getElementsByClassName('cart-items')[0]
+        var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+        for (var i = 0; i < cartItemNames.length; i++) {
+            if (cartItemNames[i].innerText == title) {
+                alert('This item is already added to the cart')
+                return
+            }
         }
-     }
-     var cartRowContents = `
+        var cartRowContents = `
         <div class="cart-item cart-column">
             <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
             <span class="cart-item-title">${title}</span>
@@ -117,22 +119,22 @@ function addItemToCart(title, price, imageSrc) {
             <input class="cart-quantity-input" type="number" value="1">
             <button class="btn btn-danger" type="button">REMOVE</button>
         </div>`
-     cartRow.innerHTML = cartRowContents
-     cartItems.append(cartRow)
-     cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
-     cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
-	}else{
-	 var cartRow = document.createElement('div')
-     cartRow.classList.add('cart-row')
-     var cartItems = document.getElementsByClassName('cart-items')[0]
-     var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
-     for (var i = 0; i < cartItemNames.length; i++) {
-        if (cartItemNames[i].innerText == title) {
-            alert('This item is already added to the cart')
-            return
+        cartRow.innerHTML = cartRowContents
+        cartItems.append(cartRow)
+        cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+        cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+    } else {
+        var cartRow = document.createElement('div')
+        cartRow.classList.add('cart-row')
+        var cartItems = document.getElementsByClassName('cart-items')[0]
+        var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+        for (var i = 0; i < cartItemNames.length; i++) {
+            if (cartItemNames[i].innerText == title) {
+                alert('This item is already added to the cart')
+                return
+            }
         }
-     }
-     var cartRowContents = `
+        var cartRowContents = `
         <div class="cart-item cart-column">
             <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
             <span class="cart-item-title">${title}</span>
@@ -140,10 +142,10 @@ function addItemToCart(title, price, imageSrc) {
         <span class="cart-price cart-column">${price}</span>
             <button class="btn btn-danger" type="button">REMOVE</button>
         </div>`
-     cartRow.innerHTML = cartRowContents
-     cartItems.append(cartRow)
-     cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
-	}
+        cartRow.innerHTML = cartRowContents
+        cartItems.append(cartRow)
+        cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    }
 }
 
 function updateCartTotal() {
@@ -151,82 +153,119 @@ function updateCartTotal() {
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
     var total = 0
     for (var i = 0; i < cartRows.length; i++) {
-        if(!arrayofAlbum.includes(cartRows[i].innerText.split("\n")[0])){
-		var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        var price = parseFloat(priceElement.innerText.replace('$', ''))
-        var quantity = quantityElement.value
-        total = total + (price * quantity)
-		}else{
-		var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var price = parseFloat(priceElement.innerText.replace('$', ''))
-		
-        total = total +  price;
-		}
-		
+        if (!arrayofAlbum.includes(cartRows[i].innerText.split("\n")[0])) {
+            var cartRow = cartRows[i]
+            var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+            var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+            var price = parseFloat(priceElement.innerText.replace('$', ''))
+            var quantity = quantityElement.value
+            total = total + (price * quantity)
+        } else {
+            var cartRow = cartRows[i]
+            var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+            var price = parseFloat(priceElement.innerText.replace('$', ''))
+
+            total = total + price;
+        }
+
     }
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
 function magnify(imgID, zoom) {
-  var img, glass, w, h, bw;
-  img = document.getElementById(imgID);
+    var img, glass, w, h, bw;
+    img = document.getElementById(imgID);
 
-  /* Create magnifier glass: */
-  glass = document.createElement("DIV");
-  glass.setAttribute("class", "img-magnifier-glass");
+    /* Create magnifier glass: */
+    glass = document.createElement("DIV");
+    glass.setAttribute("class", "img-magnifier-glass");
 
-  /* Insert magnifier glass: */
-  img.parentElement.insertBefore(glass, img);
+    /* Insert magnifier glass: */
+    img.parentElement.insertBefore(glass, img);
 
-  /* Set background properties for the magnifier glass: */
-  glass.style.backgroundImage = "url('" + img.src + "')";
-  glass.style.backgroundRepeat = "no-repeat";
-  glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
-  bw = 3;
-  w = glass.offsetWidth / 2;
-  h = glass.offsetHeight / 2;
+    /* Set background properties for the magnifier glass: */
+    glass.style.backgroundImage = "url('" + img.src + "')";
+    glass.style.backgroundRepeat = "no-repeat";
+    glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+    bw = 3;
+    w = glass.offsetWidth / 2;
+    h = glass.offsetHeight / 2;
 
-  /* Execute a function when someone moves the magnifier glass over the image: */
-  glass.addEventListener("mousemove", moveMagnifier);
-  img.addEventListener("mousemove", moveMagnifier);
+    /* Execute a function when someone moves the magnifier glass over the image: */
+    glass.addEventListener("mousemove", moveMagnifier);
+    img.addEventListener("mousemove", moveMagnifier);
 
-  /*and also for touch screens:*/
-  glass.addEventListener("touchmove", moveMagnifier);
-  img.addEventListener("touchmove", moveMagnifier);
-  function moveMagnifier(e) {
-    var pos, x, y;
-    /* Prevent any other actions that may occur when moving over the image */
-    e.preventDefault();
-    /* Get the cursor's x and y positions: */
-    pos = getCursorPos(e);
-    x = pos.x;
-    y = pos.y;
-    /* Prevent the magnifier glass from being positioned outside the image: */
-    if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
-    if (x < w / zoom) {x = w / zoom;}
-    if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
-    if (y < h / zoom) {y = h / zoom;}
-    /* Set the position of the magnifier glass: */
-    glass.style.left = (x - w) + "px";
-    glass.style.top = (y - h) + "px";
-    /* Display what the magnifier glass "sees": */
-    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
-  }
+    /*and also for touch screens:*/
+    glass.addEventListener("touchmove", moveMagnifier);
+    img.addEventListener("touchmove", moveMagnifier);
+    function moveMagnifier(e) {
+        var pos, x, y;
+        /* Prevent any other actions that may occur when moving over the image */
+        e.preventDefault();
+        /* Get the cursor's x and y positions: */
+        pos = getCursorPos(e);
+        x = pos.x;
+        y = pos.y;
+        /* Prevent the magnifier glass from being positioned outside the image: */
+        if (x > img.width - (w / zoom)) {
+            x = img.width - (w / zoom);
+        }
+        if (x < w / zoom) {
+            x = w / zoom;
+        }
+        if (y > img.height - (h / zoom)) {
+            y = img.height - (h / zoom);
+        }
+        if (y < h / zoom) {
+            y = h / zoom;
+        }
+        /* Set the position of the magnifier glass: */
+        glass.style.left = (x - w) + "px";
+        glass.style.top = (y - h) + "px";
+        /* Display what the magnifier glass "sees": */
+        glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+    }
 
-  function getCursorPos(e) {
-    var a, x = 0, y = 0;
-    e = e || window.event;
-    /* Get the x and y positions of the image: */
-    a = img.getBoundingClientRect();
-    /* Calculate the cursor's x and y coordinates, relative to the image: */
-    x = e.pageX - a.left;
-    y = e.pageY - a.top;
-    /* Consider any page scrolling: */
-    x = x - window.pageXOffset;
-    y = y - window.pageYOffset;
-    return {x : x, y : y};
-  }
+    function getCursorPos(e) {
+        var a, x = 0, y = 0;
+        e = e || window.event;
+        /* Get the x and y positions of the image: */
+        a = img.getBoundingClientRect();
+        /* Calculate the cursor's x and y coordinates, relative to the image: */
+        x = e.pageX - a.left;
+        y = e.pageY - a.top;
+        /* Consider any page scrolling: */
+        x = x - window.pageXOffset;
+        y = y - window.pageYOffset;
+        return {
+            x: x,
+            y: y
+        };
+    }
+}
+let counterOfItemShop = document.getElementsByClassName('shop-item').length;
+for (let i = 0; i < counterOfItemShop; i++) {
+	var paragraph = document.createElement('html');
+    var body = document.createElement('body');
+	body.append(document.getElementsByClassName('shop-item')[0].innerHTML);
+	paragraph.append(body)
+    var textToSave = paragraph.innerText;
+	
+    var textToSaveAsBlob = new Blob([textToSave],{
+        type: "text/html" 
+    });
+	
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    var fileNameToSaveAs = document.getElementsByClassName('shop-item-title')[i].innerHTML;
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    function destroyClickedElement(event) {
+        document.body.removeChild(event.target);
+    }
 }
